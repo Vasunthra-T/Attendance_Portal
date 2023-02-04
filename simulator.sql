10248eba0d4067a0810eefba22b0909d2a91238f
Vasunthra T <73538892+Vasunthra-T@users.noreply.github.com>
2023-02-04 14:03:10 +0530

    Update simulator.sql

--- simulator.sql
+++ simulator.sql
@@ -2,10 +2,5 @@ create table employee
 (
 id int primary key,
 emp_name varchar(40),
-emp_code varchar(30),
-phone bigint,
-email varchar (40),
-manager_id varchar(30),
-role varchar(30),
-password varchar(30)
+emp_code varchar(30)
 );
